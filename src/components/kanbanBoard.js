eface85bdb2a045db9f3d3a390dad3a1e29eff3b
Edanur Demiray <edademiray2@gmail.com>
2022-09-16 16:23:43 +0300

    Update kanbanBoard.js

--- src/components/kanbanBoard.js
+++ src/components/kanbanBoard.js
@@ -3,6 +3,10 @@ import createElement from '../utilities/createElement'
 
 const loadKanban = () => {
 
+    const todoArray = [ ]
+    const ipArray = [ ]
+    const compArray = [ ]
+
     const kbContainer = createElement({ classList: ['kanban-container'] })
     const todoDiv = createElement({ classList: ['todo-div'], innerText: 'TODO' })
     const inprogDiv = createElement({ classList: ['ip-div'], innerText: 'IN PROGRESS' })
@@ -15,30 +19,63 @@ const loadKanban = () => {
     const itemCard = createElement({ classList: ['item-card'], innerText: 'Denemecardı' })
 
 
-    const createItem = () => {
+    const createItem = (e) => {
         const itemCard = createElement({ classList: ['item-card'] })
-        const moveBtn = createElement({type:'button', classList:['move-btn'],innerText:'Move'})
+        const moveRight = createElement({ type: 'button', classList: ['move-r'], innerText: '>' })
+        const moveLeft = createElement({ type: 'button', classList: ['move-l'], innerText: '<' })
 
-        const itemValue = createElement ({classList:['item-val'],type:'input',placeholder:'give me something to-do'})
+        const itemValue = createElement({ classList: ['item-val'], type: 'input', placeholder: 'give me something to-do' })
 
         const cancelBtn = createElement({ type: 'button', classList: ['cancel-btn'], innerText: 'X' })
         const cancelItem = () => {
             itemCard.style.display = 'none'
         }
 
-
         cancelBtn.addEventListener('click', cancelItem)
-        itemCard.append(itemValue,moveBtn,cancelBtn)
-        itemCard.addEventListener('mousedown',cardMove)
-        todoDiv.append(itemCard)
+        itemCard.append(moveLeft, itemValue, moveRight, cancelBtn)
+        itemCard.addEventListener('mousedown', cardMove)
+        const selectedDiv = e.target.parentElement
+
+
+        if (selectedDiv.classList.contains('todo-div')) {
+            todoArray.push(itemCard)
+            moveLeft.classList.add('hide')
+        }
+        if (selectedDiv.classList.contains('complate-div')) {
+            moveRight.classList.add('hide')
+            ipArray.push(itemCard)
+        }
+
+
+
+        moveRight.addEventListener('click', (e) => {
+          inprogDiv.appendChild(itemCard)
+        })
+        moveLeft.addEventListener('click', () => {
+            todoDiv.appendChild(itemCard)
+        })
+
+
+        console.log(selectedDiv)
+        selectedDiv.append(itemCard)
+
+      
+
+        console.log(todoArray)
     }
 
     const cardMove = () => {
         console.log('tasınıyor')
-    } 
+    }
 
 
     todoaddBtn.addEventListener('click', createItem)
+    ipaddBtn.addEventListener('click', createItem)
+    complateaddBtn.addEventListener('click', createItem)
+
+
+
+
     todoDiv.append(todoaddBtn)
     inprogDiv.append(ipaddBtn)
     complateDiv.append(complateaddBtn)
